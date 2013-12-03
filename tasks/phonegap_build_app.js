80f3eb874e6403470612860fe7781225250ae2e7
Andrew Huling <andrewhuling@gmail.com>
2013-12-03 16:30:27 -0500

    First version of plugin using formdata

--- tasks/phonegap_build_app.js
+++ tasks/phonegap_build_app.js
@@ -8,43 +8,66 @@
 
 'use strict';
 
+var FormData = require('form-data');
+var fs = require('fs');
+var path = require('path');
+
 module.exports = function(grunt) {
+    function run(options, done) {
+        createApp(options);
+        done();
+    }
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
 
-  grunt.registerMultiTask('phonegap_build_app', 'Create and update zip file apps using PhoneGap Build.', function() {
-    // Merge task-specific and/or target-specific options with these defaults.
-    var options = this.options({
-      punctuation: '.',
-      separator: ', '
-    });
+    function createApp(options) {
+        grunt.log.ok('Uploading ' + options.archive + ' file to PhoneGap...');
+        var url = 'https://build.phonegap.com/api/v1/apps';
+        var form = new FormData();
+        form.append('data', '{"title": "' + options.appName + '", "keys": {"android": {"id": ' + options.keys.android.key_id + ', "key_pw": "' + options.keys.android.key_pw + '", "keystore_pw": "' + options.keys.android.keystore_pw + '"}, "ios": {"id": ' + options.keys.ios.key_id + ', "password": "' + options.keys.ios.password + '"}}, "private": true, "create_method": "file"}');
+        form.append('file', fs.createReadStream(options.archive));
+        form.submit('https://build.phonegap.com/api/v1/apps', function(err, res) {
+            res.resume();
+            grunt.log.ok('Upload successful!');
+        });
+    }
 
-    // Iterate over all specified file groups.
-    this.files.forEach(function(f) {
-      // Concat specified files.
-      var src = f.src.filter(function(filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+//    function updateApp(f, options) {
+//        request.put({url: 'https://build.phonegap.com/api/v1/apps/' + options.app_id, body: fs.createReadStream(f)});
+//    }
+
+//                    archive: 'build/' + grunt.option('app-name') + '.zip',
+//                    appName: grunt.option('app-name'),
+//                    user: {
+//                        email: '<%= gruntConfig.PhoneGapBuildUsername %>',
+//                        password: '<%= gruntConfig.PhoneGapBuildPassword %>'
+//                    },
+//                    keys: {
+//                        ios: {
+//                            password: '<%= gruntConfig.iOSKeyPassword %>'
+//                        },
+//                        android: {
+//                            key_pw: '<%= gruntConfig.AndroidKeyPassword %>',
+//                            keystore_pw: '<%= gruntConfig.AndroidKeystorePassword %>'
+//                        }
+//                    },
+//                    download: {
+//                        ios: 'dist/' + grunt.option('code') + '/ios.ipa',
+//                        android: 'dist/' + grunt.option('code') + '/android.apk'
+//                    }
+
+
+    grunt.registerMultiTask("phonegap-build", "Creates a ZIP archive and uploads it to build.phonegap.com to create a new build", function (args) {
+        var options = this.options({
+            timeout: 60000,
+            pollRate: 15000
+        });
+
+        if (!grunt.file.exists(options.archive)) {
+            grunt.log.fail("Archive at " + options.archive + " does not exist!");
+            return false;
         }
-      }).map(function(filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
-
-      // Write the destination file.
-      grunt.file.write(f.dest, src);
-
-      // Print a success message.
-      grunt.log.writeln('File "' + f.dest + '" created.');
+        var done = this.async();
+        run(options, done);
     });
-  });
-
 };
